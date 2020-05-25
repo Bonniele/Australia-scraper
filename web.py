0937308a2cc4fe4d730e0536dc20100495f1af21
Bonniele <43044524+Bonniele@users.noreply.github.com>
2020-05-24 22:06:40 -0500

    Update web.py

--- web.py
+++ web.py
@@ -1,22 +1,22 @@
 '''1st assesment - CNN Australia News Scaper'''
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+'''Create Chrome option'''
 option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
 
 '''Create new Insatnce of Chrome in incognito to mode'''
-browser = webdriver.Chrome (executable_path= 'C:\webdrivers\chromedriver',options = option)
-
+browser = webdriver.Chrome (executable_path= 'C:\webdrivers\chromedriver', options = option)
 
 '''Set up website url '''
 browser.get("https://www.cnn.com/australia")
     
 '''Find the news'''
-titles_element = browser.find_elements_by_xpath("//a[@class='text-bold']")
+titles_element = browser.find_elements_by_xpath("//h3[@data-analytics='Australia latest_list-xs_article_']")
 headline = [x.text for x in titles_element]
 
 '''Print Headline'''
@@ -26,7 +26,3 @@ else:
     print ("Headline: ")
     for title in headline:
         print (title)
-    
-    
-    
-    
